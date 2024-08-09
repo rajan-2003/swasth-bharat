d1f68cce3cf8b1cef5e3368514a0b6db95b6ca15
Som0503 <vermasomdutta1111@gmail.com>
2024-08-09 18:19:04 +0530

    Implement notification addition and scheduling functionality
    - Added `addNotification` function to save new notifications to the database with user ID, message, and default seen status.
    - Implemented `scheduleNotification` function to schedule notifications with a delay using a job scheduler.
    - Included error handling to return appropriate responses for success or failure.

--- server/controllers/notificationController.js
+++ server/controllers/notificationController.js
@@ -1,8 +1,35 @@
+const Notification = require("../models/notification");
+const addNotificationJob = require("../scheduler.js");
+
 // Add a new notification
-exports.addNotification = async (req, res) => {};
+exports.addNotification = async (req, res) => {const { userId, msg } = req.body;
+  try {
+    const notification = new Notification({
+      user_id: userId,
+      msg: msg,
+      seen: false,
+    });
+
+    await notification.save();
+    res
+      .status(200)
+      .json({ message: "Notification added and pushed successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to add notification" });
+  }};
 
 // Schedule a notification
-exports.scheduleNotification = async (req, res) => {};
+exports.scheduleNotification = async (req, res) => {
+   const { userId, msg, delayInMinutes = 0 } = req.body;
+
+  try {
+    await addNotificationJob(userId, msg, delayInMinutes);
+    res.status(200).json({ message: "Notification passed for verification" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Failed to schedule notification" });
+  }
+};
 
 // Update notification status
 exports.updateNotificationStatus = async (req, res) => {};
